b9f688a743c9a40ccb9b8061d0516511f0a99dcc
riku <basketball.back.0626@gmail.com>
2024-05-03 14:12:34 +0900

    プロジェクト ファイルを追加します。

---
+++ DataCollector/Model/Item.cs
@@ -0,0 +1,20 @@
+﻿namespace DataCollector.Model
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+    }
+    public class MasterItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }  // 項目名
+        public string Type { get; set; }  // データタイプ（例：string, int）
+    }
+    public class DynamicDataEntry
+    {
+        public Dictionary<string, object> Values { get; set; } = new Dictionary<string, object>();
+    }
+
+}
